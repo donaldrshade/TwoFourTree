3b634b07b41a251515018bb75d315b638f619966
Galhanone <d.dan.gar.g@gmail.com>
2017-12-09 17:32:08 -0500

    Test Commit

--- termproject/src/termproject/TF_Tree.java
+++ termproject/src/termproject/TF_Tree.java
@@ -18,7 +18,7 @@ public class TF_Tree {
     WCIT - What child is this?
     FFGTE - Find First Greater Than Equal
     Fix up for underflow
-    ->Xfer  l/R - never cause underflows
+    ->Xfer  L/R - never cause underflows
     Fusion  L/R - may cause underflow above nodes.
     
     if(LXFER_POS){
